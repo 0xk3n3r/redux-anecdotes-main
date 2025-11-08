b027b31b06f95476aee2cac0d4b611273be9a5a9
k3n3r <Baolan.Shi@colorado.edu>
2025-11-08 21:43:43 +0100

    @reduxjs/toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
-import reducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const rootReducer = combineReducers({
-  anecdotes: reducer,
-  filter: filterReducer
-})
-
-const store = createStore(rootReducer)
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
